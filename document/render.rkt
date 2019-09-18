e876cca04a10a5a83e54f51f51da51b9ceea9fb9
Caner Derici <canerderici@gmail.com>
2019-09-17 22:06:17 -0400

    update renders

--- document/render.rkt
+++ document/render.rkt
@@ -14,16 +14,16 @@
 "--------------------------- SOURCE DONE -------------------------"
 
 "-----------------------------------------------------------------"
-(render-language-nts '(LI L-obj imp-obj exp-obj
-                         I linkl-ref inst-ref
-                         ω Ω EL EI))
+(render-language-nts '(CL L-obj imp-obj exp-obj
+                          LI I linkl-ref inst-ref
+                          ω Ω EI))
 (define runtime-img (render-language Linklets))
 (define bm2 (pict->bitmap runtime-img))
 (send bm2 save-file "linklet-runtime.png" 'png)
 "------------------------- RUNTIME DONE --------------------------"
 
 "-----------------------------------------------------------------"
-(render-language-nts '(p p-top final-expr V EP))
+(render-language-nts '(p p-top V EP))
 (define program (render-language Linklets))
 (define bm3 (pict->bitmap program))
 (send bm3 save-file "linklet-program.png" 'png)
@@ -31,14 +31,21 @@
 
 "-----------------------------------------------------------------"
 (render-reduction-relation-rules '("linklet-lookup" "instance-lookup" "instance variable value" "let-inst" "instantiate linklet" "eval linklet"))
-(define reduction (render-reduction-relation -->βp #:style 'vertical))
+(define reduction (render-reduction-relation -->βp #:style 'horizontal-left-align))
 (define bm4 (pict->bitmap reduction))
 (send bm4 save-file "program-reduction.png" 'png)
 "---------------------- PROG REDUCTION DONE ----------------------"
 
 "-----------------------------------------------------------------"
+#;(metafunction-cases '("linklet-lookup" "instance-lookup" "instance variable value" "let-inst" "instantiate linklet" "eval linklet"))
+(define inst-loop-func (render-metafunction instantiate-loop))
+(define bm6 (pict->bitmap inst-loop-func))
+(send bm6 save-file "instantiate-loop.png" 'png)
+"---------------------- INSTANTIATE LOOP DONE ----------------------"
+
+"-----------------------------------------------------------------"
 (render-reduction-relation-rules #f)
-(define i-reduction (render-reduction-relation -->βi-render #:style 'horizontal-side-conditions-same-line))
+(define i-reduction (render-reduction-relation -->βi-render #:style 'vertical))
 (define bm5 (pict->bitmap i-reduction))
 (send bm5 save-file "linklet-body-reduction.png" 'png)
 "------------------------- REDUCTION DONE ------------------------"
