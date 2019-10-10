86e1c7b366f12a0567c7f26c5c672bc0f63bfaaf
Caner Derici <canerderici@gmail.com>
2019-10-10 16:39:03 -0400

    rename evaluation context

--- racket-core.rkt
+++ racket-core.rkt
@@ -16,9 +16,9 @@
   [b   ::= true false]
   [x cell ::= variable-not-otherwise-mentioned] ;; variables
   [o   ::= + * <]
-  [E   ::= hole (v ... E e ...) (o E e) (o v E) (if E e e)
-       (var-set! x E) (var-set/check-undef! x E)
-       (begin v ... E e ...) (set! x E)] ;; eval context
+  [EL   ::= hole (v ... EL e ...) (o EL e) (o v EL) (if EL e e)
+       (var-set! x EL) (var-set/check-undef! x EL)
+       (begin v ... EL e ...) (set! x EL)] ;; eval context
 
   [ρ   ::= ((x any) ...)] ;; environment
   [σ   ::= ((x any) ...)] ;; store
@@ -29,7 +29,7 @@
           (raises e-test)] ;; to be used to generate test cases (i.e. exclude closures)
   )
 
-; (render-language RC "RC.pdf" #:nts '(e v c n b x p1 p2 o E ρ σ))
+; (render-language RC "RC.pdf" #:nts '(e v c n b x p1 p2 o EL ρ σ))
 
 (define-metafunction RC
   [(let-values (((x) e) ...) e_body)
@@ -61,47 +61,47 @@
   (reduction-relation
    RC
    #:domain (e ρ σ)
-   (--> [(in-hole E (raises e)) ρ σ]
+   (--> [(in-hole EL (raises e)) ρ σ]
         [(raises e) ρ σ] "error")
-   (--> [(in-hole E x) ρ σ]
-        [(in-hole E (lookup σ x_1)) ρ σ] "lookup"
+   (--> [(in-hole EL x) ρ σ]
+        [(in-hole EL (lookup σ x_1)) ρ σ] "lookup"
         (where x_1 ,(term (lookup ρ x))))
 
-   (--> [(in-hole E (var-ref x)) ρ σ]
-        [(in-hole E v) ρ σ]
+   (--> [(in-hole EL (var-ref x)) ρ σ]
+        [(in-hole EL v) ρ σ]
         (where v (lookup σ (lookup ρ x)))
         "var-ref")
-   (--> [(in-hole E (var-ref/no-check x)) ρ σ]
-        [(in-hole E v) ρ σ]
+   (--> [(in-hole EL (var-ref/no-check x)) ρ σ]
+        [(in-hole EL v) ρ σ]
         (where v (lookup σ (lookup ρ x)))
         "var-ref/no-check") ; for now the same with var-ref
-   (--> [(in-hole E (var-set! x v)) ρ σ]
-        [(in-hole E (void)) ρ (extend σ (cell_var) (v))]
+   (--> [(in-hole EL (var-set! x v)) ρ σ]
+        [(in-hole EL (void)) ρ (extend σ (cell_var) (v))]
         (where cell_var (lookup ρ x))
         "var-set!")
-   (--> [(in-hole E (var-set/check-undef! x v)) ρ σ]
-        [(in-hole E (void)) ρ (extend σ (cell_var) (v))]
+   (--> [(in-hole EL (var-set/check-undef! x v)) ρ σ]
+        [(in-hole EL (void)) ρ (extend σ (cell_var) (v))]
         (where cell_var (lookup ρ x))
         (where v_var (lookup σ cell_var)) ; to make sure it's there
         "var-set/check-undef!") ; for now the same with var-set!
 
-   (--> [(in-hole E (lambda (x ...) e)) ρ σ]
-        [(in-hole E (closure x ... e ρ)) ρ σ] "closure")
-   (--> [(in-hole E (set! x v)) ρ σ]
-        [(in-hole E (void)) ρ (extend σ (x_1) (v))]
+   (--> [(in-hole EL (lambda (x ...) e)) ρ σ]
+        [(in-hole EL (closure x ... e ρ)) ρ σ] "closure")
+   (--> [(in-hole EL (set! x v)) ρ σ]
+        [(in-hole EL (void)) ρ (extend σ (x_1) (v))]
         (side-condition (not (equal? (term (raises ,(term x))) (term (lookup ρ x)))))
         (where x_1 ,(term (lookup ρ x))) "set!")
-   (--> [(in-hole E (begin v_1 ... v_n)) ρ σ]
-        [(in-hole E v_n) ρ σ] "begin")
-   (--> [(in-hole E (if v_0 e_1 e_2)) ρ σ]
-        [(in-hole E e_1) ρ σ]
+   (--> [(in-hole EL (begin v_1 ... v_n)) ρ σ]
+        [(in-hole EL v_n) ρ σ] "begin")
+   (--> [(in-hole EL (if v_0 e_1 e_2)) ρ σ]
+        [(in-hole EL e_1) ρ σ]
         (side-condition (not (equal? (term v_0) (term false)))) "if-true")
-   (--> [(in-hole E (if false e_1 e_2)) ρ σ]
-        [(in-hole E e_2) ρ σ] "if-false")
-   (--> [(in-hole E (o v_1 v_2 ...)) ρ σ]
-        [(in-hole E (δ (o v_1 v_2 ...))) ρ σ] "δ")
-   (--> [(in-hole E ((closure x ..._n e ρ_1) v ..._n)) ρ_2 σ]
-        [(in-hole E e) (extend ρ_1 (x ...) (x_2 ...)) (extend σ (x_2 ...) (v ...))] "βv"
+   (--> [(in-hole EL (if false e_1 e_2)) ρ σ]
+        [(in-hole EL e_2) ρ σ] "if-false")
+   (--> [(in-hole EL (o v_1 v_2 ...)) ρ σ]
+        [(in-hole EL (δ (o v_1 v_2 ...))) ρ σ] "δ")
+   (--> [(in-hole EL ((closure x ..._n e ρ_1) v ..._n)) ρ_2 σ]
+        [(in-hole EL e) (extend ρ_1 (x ...) (x_2 ...)) (extend σ (x_2 ...) (v ...))] "βv"
         (where (x_2 ...) ,(variables-not-in (term e) (term (x ...)))))))
 
 (define-metafunction RC
