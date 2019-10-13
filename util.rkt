5b4c8aaa49a36a6ba7e8a2012590cd44e6c20aaf
Caner Derici <canerderici@gmail.com>
2019-10-13 13:04:33 -0400

    minor rename

--- util.rkt
+++ util.rkt
@@ -27,21 +27,21 @@
 ;; substitute instance
 
 (define-metafunction Linklets
-  subst-inst-one : x LI p-top -> p-top
-  [(subst-inst-one x LI (instantiate-linklet linkl-ref inst-ref_bef ... x inst-ref_aft ...))
+  substitute-instance : x LI p-top -> p-top
+  [(substitute-instance x LI (instantiate-linklet linkl-ref inst-ref_bef ... x inst-ref_aft ...))
    (instantiate-linklet linkl-ref inst-ref_bef ... LI inst-ref_aft ...)]
-  [(subst-inst-one x LI (instantiate-linklet linkl-ref inst-ref_bef ... x inst-ref_aft ... #:target inst-ref_tar))
+  [(substitute-instance x LI (instantiate-linklet linkl-ref inst-ref_bef ... x inst-ref_aft ... #:target inst-ref_tar))
    (instantiate-linklet linkl-ref inst-ref_bef ... LI inst-ref_aft ... #:target inst-ref_tar)]
-  [(subst-inst-one x LI (instantiate-linklet linkl-ref inst-ref ... #:target x))
+  [(substitute-instance x LI (instantiate-linklet linkl-ref inst-ref ... #:target x))
    (instantiate-linklet LI inst-ref ... #:target LI)]
-  [(subst-inst-one x LI (let-inst x_1 I p-top))
+  [(substitute-instance x LI (let-inst x_1 I p-top))
    (let-inst x_1 I_s p-top_1)
-   (where I_s (subst-inst-one x LI I))
+   (where I_s (substitute-instance x LI I))
    (where p-top_1 (substitute-one x L-obj p-top))]
-  [(subst-inst-one x LI p-top) p-top])
+  [(substitute-instance x LI p-top) p-top])
 
 (define-metafunction Linklets
-  [(subst-inst x LI () (p-top ...)) (p-top ...)]
-  [(subst-inst x LI (p-top_1 p-top ...) (p-top_new ...))
-   (subst-inst x LI (p-top ...) (p-top_new ... p-top_new1))
-   (where p-top_new1 (subst-inst-one x LI p-top_1))])
+  [(substitute-instance-multi x LI () (p-top ...)) (p-top ...)]
+  [(substitute-instance-multi x LI (p-top_1 p-top ...) (p-top_new ...))
+   (substitute-instance-multi x LI (p-top ...) (p-top_new ... p-top_new1))
+   (where p-top_new1 (substitute-instance x LI p-top_1))])
