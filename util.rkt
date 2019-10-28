69bf797420571ddd10c94a5b831aeb70b55613fd
Caner Derici <canerderici@gmail.com>
2019-10-28 17:28:35 -0400

    some cleaning

--- util.rkt
+++ util.rkt
@@ -28,27 +28,3 @@
   [(substitute-linklet x L-obj (p-top ...))
    (p-top_new ...)
    (where (p-top_new ...) ((substitute-one x L-obj p-top) ...))])
-
-;; substitute instance
-
-#;(define-metafunction Linklets
-  substitute-instance : x LI p-top -> p-top
-  [(substitute-instance x LI (instantiate-linklet linkl-ref x_bef ... x x_aft ...))
-   (instantiate-linklet linkl-ref x_bef ... LI x_aft ...)]
-  [(substitute-instance x LI (instantiate-linklet linkl-ref x_bef ... x x_aft ... #:target x_tar))
-   (instantiate-linklet linkl-ref x_bef ... LI x_aft ... #:target x_tar)]
-  [(substitute-instance x LI (instantiate-linklet linkl-ref x ... #:target x))
-   (instantiate-linklet linkl-ref x ... #:target LI)]
-  [(substitute-instance x LI (let-inst x_1 I p-top))
-   (let-inst x_1 I_s p-top_1)
-   (where I_s (substitute-instance x LI I))
-   (where p-top_1 (substitute-instance x LI p-top))]
-  [(substitute-instance x LI (instance-variable-value x x_var))
-   (instance-variable-value LI x_var)]
-  [(substitute-instance x LI p-top) p-top])
-
-#;(define-metafunction Linklets
-  [(substitute-instance-multi x LI () (p-top ...)) (p-top ...)]
-  [(substitute-instance-multi x LI (p-top_1 p-top ...) (p-top_new ...))
-   (substitute-instance-multi x LI (p-top ...) (p-top_new ... p-top_new1))
-   (where p-top_new1 (substitute-instance x LI p-top_1))])
